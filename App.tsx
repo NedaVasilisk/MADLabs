f68f4dd399e26745f2caab141e37f789eca9c531
NedaVasilisk <120412944+NedaVasilisk@users.noreply.github.com>
2025-05-02 19:25:55 +0300

    feat: setup navigation and define screen routes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import EditorScreen from './screens/EditorScreen';
+import { RootStackParamList } from './types/RootStackParamList';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="FileManager" component={HomeScreen} />
+                <Stack.Screen name="Editor" component={EditorScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ types/RootStackParamList.ts
@@ -0,0 +1,4 @@
+export type RootStackParamList = {
+    FileManager: undefined;
+    Editor: { path: string };
+};
